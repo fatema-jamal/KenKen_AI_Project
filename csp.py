5e49bcf7c9d2202f197348f9bf6df72779b0d35e
Salma Hamdy <40768679+salma39@users.noreply.github.com>
2022-06-02 22:45:46 +0200

    Update csp.py

--- csp.py
+++ csp.py
@@ -1,1 +1,23 @@
 
+class CSP():
+    def __init__(self, variables, domains, neighbors, constraints):
+        
+        variables = variables or list(domains.keys())
+
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.initial = ()
+        self.curr_domains = None
+        self.nassigns = 0
+
+    def assign(self, var, val, assignment):
+
+        assignment[var] = val
+        self.nassigns += 1
+
+    def unassign(self, var, assignment):
+
+        if var in assignment:
+            del assignment[var]
